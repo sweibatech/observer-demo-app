dd2b694f982f7031b6d47a67733addf7fe80781a
Anton Sukharev <sweibatech@gmail.com>
2025-03-12 14:11:28 +0300

    fix: code fixer

--- app.php
+++ app.php
@@ -1,16 +1,17 @@
 <?php
+
 declare(strict_types=1);
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 use ObserverDemoApp\Observers\MailObserver;
 use ObserverDemoApp\Observers\ManagerObserver;
 use ObserverDemoApp\Venue\EventsEnum;
 use ObserverDemoApp\Venue\VisitorFactory;
 
-$visitor = VisitorFactory::create("John Doe");
+$visitor = VisitorFactory::create('John Doe');
 
-$visitor->addObserver(new ManagerObserver());
-$visitor->addObserver(new MailObserver());
+$visitor->addObserver(new ManagerObserver);
+$visitor->addObserver(new MailObserver);
 
 $visitor->enterVenue();
 
